eb1bd3f80155e1ffb05b93d41f9d2af0a082fd05
WeiRuJian <824041965@qq.com>
2020-06-09 17:19:52 +0800

    Initial Commit

---
+++ Hacking_SwiftUI/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Hacking_SwiftUI
+//
+//  Created by 韦儒健 on 2020/6/9.
+//  Copyright © 2020 WeiRuJian. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
